df9ad4fdb203180b8583b84cba7ff187869cb572
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-03-31 02:54:34 -0300

    Clase N°25: Tipo básicos.

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -1,5 +1,31 @@
-console.log('Hello, TypeScript');
-function add(a, b) {
-    return a + b;
-}
-var sum = add(2, 3);
+// Boolean
+var muted = true;
+muted = false;
+// Números
+var numerador = 42;
+var denominador = 6;
+var resultado = numerador / denominador;
+// String
+var nombre = 'Galeed';
+var saludo = "Mi nombres es " + nombre + ".";
+// Arreglos
+var people = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+var peopleAndNumber = [];
+peopleAndNumber.push(nombre);
+peopleAndNumber.push(100);
+// enum
+var Color;
+(function (Color) {
+    Color["Rojo"] = "Rojo";
+    Color["Verde"] = "Verder";
+    Color["Azul"] = "Azul";
+    Color["Amarrillo"] = "Amarrillo";
+})(Color || (Color = {}));
+var colorFavorito = Color.Verde;
+console.log("Mi color favorito es " + colorFavorito);
+// any
+var comodin = 'Joker';
+comodin = { type: 'Wildcard' };
+// Object
+var someObject = { type: 'Wildcard' };
--- exercises/typescript/index.ts
+++ exercises/typescript/index.ts
@@ -1,7 +1,37 @@
-console.log('Hello, TypeScript');
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add (a:number, b:number) {
-    return a + b;
+// Números
+let numerador: number= 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Galeed';
+let saludo = `Mi nombres es ${nombre}.`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+
+let peopleAndNumber: Array< string | number > = [];
+peopleAndNumber.push(nombre);
+peopleAndNumber.push(100);
+
+// enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verder",
+    Azul = "Azul",
+    Amarrillo = "Amarrillo",
 }
+let colorFavorito: Color = Color.Verde;
+console.log(`Mi color favorito es ${colorFavorito}`);
 
-const sum = add(2, 3);
+// any
+let comodin: any = 'Joker';
+comodin = {type: 'Wildcard'}
+
+// Object
+let someObject: Object = {type: 'Wildcard'}
